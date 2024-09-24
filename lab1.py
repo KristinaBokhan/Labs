50561a703424ebcb9e2cf8950ef93a18597d51dd
KristinaBokhan <74237538+KristinaBokhan@users.noreply.github.com>
2024-09-24 13:55:05 +0300

    Removed comments

--- lab1.py
+++ lab1.py
@@ -6,20 +6,16 @@ class Storage(ABC):
     def connect(self):
         pass
 
-# Локальний диск
 class LocalDisk(Storage):
     def connect(self):
         print("Connected to Local Disk")
 
-# Сховище Amazon S3
 class AmazonS3(Storage):
     def connect(self):
         print("Connected to Amazon S3")
 
-# Клас Одинак для керування підключенням користувачів до сховища
 class StorageManager:
-    _instance = None  # Статична змінна для збереження єдиного екземпляра
-
+    _instance = None  
     def __new__(cls):
         if cls._instance is None:
             cls._instance = super(StorageManager, cls).__new__(cls)
@@ -35,17 +31,15 @@ class StorageManager:
         else:
             print("No storage selected!")
 
-# Основна частина
+
 if __name__ == "__main__":
-    # Отримуємо екземпляр StorageManager (Одинак)
+
     manager = StorageManager()
 
-    # Підключаємо користувача до локального диску
     local_disk = LocalDisk()
     manager.set_storage(local_disk)
     manager.connect_to_storage()
 
-    # Підключаємо користувача до Amazon S3
     amazon_s3 = AmazonS3()
     manager.set_storage(amazon_s3)
     manager.connect_to_storage()
